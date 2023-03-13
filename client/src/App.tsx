fba1a8a190a65b0e76b5d52155a6ff1f3f2d3080
Ojowa Odunayo <hunchoweb@gmail.com>
2023-03-13 16:26:20 +0100

    Updated fetch function in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -58,7 +58,7 @@ function App() {
 
             if (profileObj) {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/users",
+                    "https://yarigaa-dashboard.netlify.app/api/v1/users",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
